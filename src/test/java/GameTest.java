a34ce36ef8f09be5899d7c156c18dd28855ca3c0
Tom Spencer <tom@homeflow.co.uk>
2020-02-18 08:24:46 +0000

    Add tests for spare, strike and perfect game

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -13,17 +13,31 @@ public class GameTest {
 
     @Test
     public void canScoreGutterGame(){
-        for (int i = 0; i < 20; i++){
-            game.roll(0);
-        }
+        game.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         assertEquals(0, game.score());
     }
 
     @Test
     public void canScoreGameOfOnes(){
-        for (int i = 0; i < 20; i++){
-            game.roll(1);
-        }
+        game.roll(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
         assertEquals(20, game.score());
+    }
+
+    @Test
+    public void canScoreSpareFollowedByThree(){
+        game.roll(5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    public void canScoreStrikeFollowedByThreeThenThree(){
+        game.roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertEquals(22, game.score());
+    }
+
+    @Test
+    public void canScorePerfectGame(){
+        game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+        assertEquals(300, game.score());
     }
 }
